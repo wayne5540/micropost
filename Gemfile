be0b9652f32d7c972b5d65f10007a07f21ff81f2
Airson <airson@server>
2013-08-17 16:14:23 +0800

    chapter9 finished

--- Gemfile
+++ Gemfile
@@ -4,8 +4,14 @@ ruby '2.0.0'
 
 gem 'rails', '4.0.0'
 gem 'bootstrap-sass', '2.3.2.0'
-gem 'bcrypt-ruby', '3.0.1'
 #password encrypted gem
+gem 'bcrypt-ruby', '3.0.1'
+#fake users
+gem 'faker', '1.1.2'
+#add paginate and it's css
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
+
 group :development, :test do
   gem 'sqlite3', '1.3.7'
   gem 'rspec-rails', '2.13.1'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,19 @@
 class UsersController < ApplicationController
+  #let user access edit and update action only if they are signed in.
+  before_action :sign_in_user, only: [:indes, :edit, :update, :deatroy
+  #let user change their own data only.
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   def show
   	@user = User.find(params[:id])
   end
+
   def new
   	@user = User.new
   end
+
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)  
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Microposts"
@@ -16,9 +23,51 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    #don't need to find user because the correct_user before filter defines @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile update"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
 
   private
+  
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
